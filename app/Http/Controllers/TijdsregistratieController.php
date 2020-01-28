329989f661031145c8261d41ce978fd7f02f901e
Midas Van Herck <midas-vanherck@hotmail.com>
2020-01-28 15:45:12 +0100

    Update TijdsregistratieController.php

--- app/Http/Controllers/TijdsregistratieController.php
+++ app/Http/Controllers/TijdsregistratieController.php
@@ -6,21 +6,61 @@ use Illuminate\Http\Request;
 
 class TijdsregistratieController extends Controller
 {
-    //
 
     public function postTijdsregistratie(Request $request){
         $data = $request->all();
 
+        //var_dump($data);
+
+        $gebruikerId = $data['gebruikerId'];
         $verenigingId = $data['verenigingId'];
         $evenementId = $data['evenementId'];
 
-        $evenementVereniging = \App\EvenementVereniging::where(['vereniging_id'=> $verenigingId,
-        'evenement_id'=>$evenementId])->first();
 
-        dd($evenementVereniging);
 
-        //$vorigeTijdsregistratie = \App\Tijdsregistratie::where($gebruiker_id, $data->gebruikerId)->orderBy('id', 'desc')->first();
+        //$evenementVereniging = \App\EvenementVereniging::where(['vereniging_id'=> $verenigingId,'evenement_id'=>$evenementId])->first();
 
+
+
+        $vorigeTijdsregistratie = \App\Tijdsregistratie::where(['gebruiker_id'=> $gebruikerId,'evenement_id'=>$evenementId, 'vereniging_id' => $verenigingId])->orderBy('id', 'desc')->first();
+
+        if(!$data['checkInTime']==null){
+            if($vorigeTijdsregistratie==null)
+            {
+                $registratie->gebruiker_id = $gebruikerId
+                $registratie->evenement_id = $evenementId;
+                $registratie->vereniging_id = $verenigingId;
+                $registratie->checkIn = $data['checkInTime'];
+                \App\Tijdsregistratie::create($registratie);
+            }
+            elseif($vorigeTijdsregistratie->checkIn==null) {
+                $registratie->gebruiker_id = $gebruikerId
+                $registratie->evenement_id = $evenementId;
+                $registratie->vereniging_id = $verenigingId;
+                $registratie->checkIn = $data['checkInTime'];
+                \App\Tijdsregistratie::create($registratie);
+                return json($vorigeTijdsregistratie);
+            }
+            elseif($vorigeTijdsregistratie->checkOut==null){
+                $registratie->gebruiker_id = $gebruikerId
+                $registratie->evenement_id = $evenementId;
+                $registratie->vereniging_id = $verenigingId;
+                $registratie->checkIn = $data['checkInTime'];
+                \App\Tijdsregistratie::create($registratie);
+                return json($vorigeTijdsregistratie);
+            }
+        }
+        else{
+            if($vorigeTijdsregistratie->checkOut==null){
+
+            }
+            else{
+
+            }
+
+        }
+
+        //var_dump($vorigeTijdsregistratie);
 
 
         //$tijdsregistratie = \App\Tijdsregistratie::create($data);
