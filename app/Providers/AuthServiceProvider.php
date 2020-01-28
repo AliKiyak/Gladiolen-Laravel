fa2629254c0819437396e53587da5bd5a515c8cd
Joustaman <jonascroonenborghs@hotmail.com>
2020-01-28 11:24:51 +0100

    Client provider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,7 +27,7 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
         Passport::routes();
-        Passport::personalAccessClientId('3');
+        Passport::personalAccessClientId('1');
         //
     }
 }
