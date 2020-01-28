081eaeda4074ba7601be7139651f3fc6c2ca2cf2
Ali Kiyak <alikiyak@outlook.com>
2020-01-28 13:06:56 +0100

    api.php

--- routes/api.php
+++ routes/api.php
@@ -1,65 +1,5 @@
 <?php
 
-<<<<<<< Updated upstream
-    use Illuminate\Http\Request;
-
-    /*
-    |--------------------------------------------------------------------------
-    | API Routes
-    |--------------------------------------------------------------------------
-    |
-    | Here is where you can register API routes for your application. These
-    | routes are loaded by the RouteServiceProvider within a group which
-    | is assigned the "api" middleware group. Enjoy building your API!
-    |
-    */
-    Route::post('login', 'GebruikerController@login');
-    Route::post('/gebruiker/registreerverantwoordelijke', 'GebruikerController@registreerVerantwoordelijke');
-    Route::post('/vereniging', 'VerenigingController@registreer');
-    Route::post('/tshirt', 'TshirtController@create');
-    Route::post('/vereniging', 'VerenigingController@registreer');
-
-    //Voor mobile
-    Route::get('/evenement/getGebruikersFromEvenement/{id}', 'EvenementController@getGebruikersFromEvenement');
-    Route::get('/actieveEvenementen', 'EvenementController@getActieveEvenementen');
-
-    Route::group(['middleware' => 'auth:api'], function () {
-        // Tshirt
-        Route::get('/tshirt', 'TshirtController@index');
-        Route::put('/tshirt/{id}', 'TshirtController@updateLidGeslachtEnMaat');
-
-        //Gebruiker
-        Route::post('/gebruiker/addlid', 'GebruikerController@addLid');
-        Route::delete('/gebruiker/deletelid/{id}', 'GebruikerController@deleteLid');
-        Route::get('/gebruiker/getlid/{id}', 'GebruikerController@getLid');
-        Route::put('/gebruiker/updatelid/{id}', 'GebruikerController@updateLid');
-        Route::post('/gebruiker/registreergebruiker', 'GebruikerController@registreerGebruiker');
-        Route::get('/gebruiker/getGebruiker/{id}', 'GebruikerController@getGebruiker');
-
-        // Evenement
-        Route::get('/evenement', 'EvenementController@index');
-        Route::post('/evenement', 'EvenementController@registreer');
-        Route::put('/evenement/{id}', 'EvenementController@updateEvenement');
-
-        // Vereniging
-        Route::get('/vereniging', 'VerenigingController@index');
-        Route::get('/vereniging/getVereniging/{id}', 'VerenigingController@getVereniging');
-        Route::get('/vereniging/verenigingmetleden', 'VerenigingController@getVerenigingMetLeden');
-        Route::get('/vereniging/verenigingingelogd', 'VerenigingController@getVerenigingVanIngelogdeGebruiker');
-        Route::put('/vereniging/{id}', 'VerenigingController@updateVereniging');
-
-        // EvenementVereniging
-        Route::get('/evenementVereniging/getVerenigingenByEvenementId/{evenementId}', 'EvenementVerenigingController@getVerenigingenByEvenementId');
-        Route::post('/evenementVereniging/postEvenementVereniging', 'EvenementVerenigingController@registreerEvenementVereniging');
-
-        // Rol
-        Route::get('/rol', 'RolController@index');
-
-    });
-
-
-
-=======
 use Illuminate\Http\Request;
 
 /*
@@ -116,5 +56,5 @@ Route::group(['middleware' => 'auth:api'], function(){
 
 Route::get('/evenement/getGebruikersFromEvenement/{id}', 'EvenementController@getGebruikersFromEvenement');
 Route::get('/actieveEvenementen','EvenementController@getActieveEvenementen');
->>>>>>> Stashed changes
+
 
