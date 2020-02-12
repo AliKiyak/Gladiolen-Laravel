d625e5442f7148b349c989b99cde1d37f2389419
Joustaman <jonascroonenborghs@hotmail.com>
2020-02-12 15:46:35 +0100

    Fix

--- app/Http/Controllers/GebruikerController.php
+++ app/Http/Controllers/GebruikerController.php
@@ -109,15 +109,12 @@ class GebruikerController extends Controller
     public function addLid(Request $request)
     {
         $data = $request->all();
-        if(\App\Gebruiker::where('rijksregisternr', $data['rijksregisternr'])->first() == null) {
             $gebruiker = \App\Gebruiker::create($data);
             $rol = \App\Rol::find(4);
 
             $gebruiker->rol()->associate($rol);
             $gebruiker->save();
-        } else {
-            $gebruiker = \App\Gebruiker::where('rijksregisternr', $data['rijksregisternr'])->first();
-        }
+
         $user = Auth::user();
         $vereniging = \App\Vereniging::where('hoofdverantwoordelijke', $user->id)->first();
         $vereniging->gebruikers()->save($gebruiker);
